2ae46b0085acb27fb926847e725cd695e45e1296
DiegoTUI <dlafuente@tuitravel-ad.com>
2014-01-07 16:18:20 +0100

    Initial Commit

---
+++ Radar/TUIAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TUIAppDelegate.h
+//  Radar
+//
+//  Created by Diego Lafuente on 07/01/14.
+//  Copyright (c) 2014 Tui Travel A&D. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TUIAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ RadarTests/RadarTests.m
@@ -0,0 +1,34 @@
+//
+//  RadarTests.m
+//  RadarTests
+//
+//  Created by Diego Lafuente on 07/01/14.
+//  Copyright (c) 2014 Tui Travel A&D. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface RadarTests : XCTestCase
+
+@end
+
+@implementation RadarTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
