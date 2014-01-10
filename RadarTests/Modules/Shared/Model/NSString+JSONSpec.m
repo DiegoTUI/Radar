12d02edf68f44e754c45a6836ef03e2bc92d8170
DiegoTUI <dlafuente@tuitravel-ad.com>
2014-01-10 10:49:14 +0100

    issue #3: added NSString+JSONSpec

---
+++ RadarTests/Modules/Shared/Model/NSString+JSONSpec.m
@@ -0,0 +1,34 @@
+//
+//  NSString+JSONSpec.m
+//  Radar
+//
+//  Created by Diego Lafuente on 1/7/14.
+//  Copyright (c) 2013 Tui Travel A&D. All rights reserved.
+//
+
+#import "Kiwi.h"
+#import "NSString+JSON.h"
+
+
+SPEC_BEGIN(NSString_JSONSpec)
+
+describe(@"NSString+JSON", ^{
+    
+    describe(@"When parsing a JSON", ^{
+        it(@"should produce a valid dictionary if a valid JSON string is provided", ^{
+            NSString *testString = @"{\"key1\":\"value1\", \n\"key2\":\"value2\"}";
+            NSDictionary *dictionary = [testString JSONDictionary];
+            [[dictionary shouldNot] beNil];
+            [[dictionary[@"key1"] should] equal:@"value1"];
+            [[dictionary[@"key2"] should] equal:@"value2"];
+        });
+        
+        it(@"should return nil if the JSON string is not valid", ^{
+            NSString *testString = @"malformed JSON";
+            NSDictionary *dictionary = [testString JSONDictionary];
+            [[dictionary should] beNil];
+        });
+    });
+});
+
+SPEC_END
