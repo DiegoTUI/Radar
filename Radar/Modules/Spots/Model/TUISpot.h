e1839bf8b7bf4d11d69c901f745ec13207df7fea
DiegoTUI <dlafuente@tuitravel-ad.com>
2014-01-14 12:38:33 +0100

    added few more images. Added blank TUISpot. Sorted the files.

---
+++ Radar/Modules/Spots/Model/TUISpot.h
@@ -0,0 +1,13 @@
+//
+//  TUISpot.h
+//  Radar
+//
+//  Created by Diego Lafuente on 14/01/14.
+//  Copyright (c) 2014 Tui Travel A&D. All rights reserved.
+//
+
+#import "TUIBaseModel.h"
+
+@interface TUISpot : TUIBaseModel
+
+@end
