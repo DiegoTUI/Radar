d7f90dd39d27979a6bedf9bd51695c09c6ca0115
DiegoTUI <dlafuente@tuitravel-ad.com>
2014-01-21 18:32:49 +0100

    added TUIFoursquareVenueParser and tests

---
+++ Radar/Modules/Spots/Model/TUIFoursquareVenueParser.h
@@ -0,0 +1,16 @@
+//
+//  TUIFoursquareVenueParser.h
+//  Radar
+//
+//  Created by Diego Lafuente on 21/01/14.
+//  Copyright (c) 2014 Tui Travel A&D. All rights reserved.
+//
+
+#import "TUIParser.h"
+
+/**
+ TUIAtlasTicketParser parses ms-venues API responses into valid TUIFourSquareVenue objects
+ */
+@interface TUIFoursquareVenueParser : NSObject <TUIParser>
+
+@end
