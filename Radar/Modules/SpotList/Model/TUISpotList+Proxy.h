b6bd041f23a5859f7e622392f093ae99770d0bae
DiegoTUI <dlafuente@tuitravel-ad.com>
2014-01-22 09:18:11 +0100

    added TUISpotList+Proxy. Still empty.

--- Radar/Modules/SpotList/Model/TUISpotList+Proxy.h
+++ Radar/Modules/SpotList/Model/TUISpotList+Proxy.h
@@ -7,7 +7,28 @@
 //
 
 #import "TUISpotList.h"
+#import "TUIProxy.h"
 
-@interface TUISpotList (Proxy)
+@interface TUISpotList (Proxy) <TUIProxy>
+
+/**
+ @methodName spotsForLatitude:longitude:radius:completion:failure:
+ @abstract Requests a list of spots with a specfic location and radius
+ @discussion This method will send a request to the API endpoint requesting a 
+ list of spots with a certain location and radius the TUISpotList object will be
+ returned in the compleition block. If there is an error the failure block will
+ be triggered with an NSError object.
+ 
+ @param latitude The latitude of the request
+ @param longitude The longitude of the request
+ @param radius The radius of the request
+ @param completion The compleition block containing the LRHotel object
+ @param failure The failure block containg the NSError that occured
+ */
++ (void)spotsForLatitude:(CGFloat)latitude
+               longitude:(CGFloat)longitude
+                  radius:(CGFloat)radius
+              completion:(void(^)(TUISpotList *spotList))completion
+                 failure:(void(^)(NSError *error))failure;
 
 @end
--- Radar/Modules/SpotList/Model/TUISpotList+Proxy.m
+++ Radar/Modules/SpotList/Model/TUISpotList+Proxy.m
@@ -8,6 +8,33 @@
 
 #import "TUISpotList+Proxy.h"
 
+static NSMutableSet *spotListURLs;
+
 @implementation TUISpotList (Proxy)
+
+
+#pragma mark - Requests -
+
++ (void)spotsForLatitude:(CGFloat)latitude
+               longitude:(CGFloat)longitude
+                  radius:(CGFloat)radius
+              completion:(void(^)(TUISpotList *spotList))completion
+                 failure:(void(^)(NSError *error))failure
+{
+}
+
+
+#pragma mark - Cancellation -
+
++ (void)cancelNetworkRequests
+{
+    /*for(NSURL *url in hotelURLs)
+    {
+        NSString *path = [url path];
+        [[LRClient sharedClient] cancelAllHTTPOperationsWithMethod:GET_REQUEST_METHOD path:path];
+    }
+    
+    [hotelURLs removeAllObjects];*/
+}
 
 @end
