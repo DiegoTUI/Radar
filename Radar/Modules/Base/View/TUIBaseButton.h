6f0f1b4167613d0bcc90232bc80682735f6c20a9
DiegoTUI <dlafuente@tuitravel-ad.com>
2014-01-29 13:51:47 +0100

    added TUIBaseButton

---
+++ Radar/Modules/Base/View/TUIBaseButton.h
@@ -0,0 +1,14 @@
+//
+//  TUIBaseButton.h
+//  Radar
+//
+//  Created by Diego Lafuente on 29/01/14.
+//  Copyright (c) 2014 Tui Travel A&D. All rights reserved.
+//
+
+/**
+ TUIBaseButton is a base class for all the buttons
+ */
+@interface TUIBaseButton : UIButton
+
+@end
